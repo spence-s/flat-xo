22ac4716a258cb7e4376fff597dd86b0b3def428
Spencer Snyder <sasnyde2@gmail.com>
2023-03-07 17:23:58 -0500

    chore: more testing

--- test/lint-files/no-config.ts
+++ test/lint-files/no-config.ts
@@ -6,19 +6,41 @@ import {lintFiles} from '../../lib/index.js';
 const __dirname = path.dirname(url.fileURLToPath(new URL(import.meta.url)));
 
 test('lints js file with no config', async (t) => {
-  const cwd = path.resolve(__dirname, '..', 'fixtures', 'no-config-js');
+  const cwd = path.resolve(
+    __dirname,
+    '..',
+    'fixtures',
+    'no-config',
+    'no-config-js',
+  );
 
-  const results = await lintFiles('', {cwd});
+  const {results} = await lintFiles('', {cwd});
 
-  t.is(results.errorCount, 1);
-  t.is(results?.messages?.[0]?.messageId, 'missingSemi');
+  t.is(results.length, 1);
+  t.is(results?.[0]?.messages?.[0]?.messageId, 'missingSemi');
 });
 
 test('lints ts file with no config', async (t) => {
-  const cwd = path.resolve(__dirname, '..', 'fixtures', 'no-config-ts');
+  const cwd = path.resolve(
+    __dirname,
+    '..',
+    'fixtures',
+    'no-config',
+    'no-config-ts',
+  );
 
-  const results = await lintFiles('', {cwd});
+  const {results} = await lintFiles('', {cwd});
 
-  t.is(results.errorCount, 1);
-  t.is(results?.messages?.[0]?.messageId, 'missingSemi');
+  t.is(results.length, 1);
+  t.is(results?.[0]?.messages?.[0]?.messageId, 'missingSemi');
+});
+
+test('lints js and ts files simultaneously with no config', async (t) => {
+  const cwd = path.resolve(__dirname, '..', 'fixtures', 'no-config');
+
+  const {results} = await lintFiles('', {cwd});
+
+  t.is(results.length, 2);
+  t.is(results?.[0]?.messages?.[0]?.messageId, 'missingSemi');
+  t.is(results?.[1]?.messages?.[0]?.messageId, 'missingSemi');
 });
