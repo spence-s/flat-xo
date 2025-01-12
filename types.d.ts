4dba9b552caa1f61fb2261a79858f0d268235dea
Spencer Snyder <sasnyde2@gmail.com>
2025-01-12 18:04:01 -0500

    chore: remove commented code

--- types.d.ts
+++ types.d.ts
@@ -5,13 +5,6 @@ declare module 'eslint-plugin-ava' {
   export default plugin;
 }
 
-// declare module 'eslint-plugin-unicorn' {
-//   import {type ESLint} from 'eslint';
-
-//   const plugin: ESLint.Plugin;
-//   export default plugin;
-// }
-
 declare module 'eslint-plugin-import-x' {
   import {type ESLint} from 'eslint';
 
